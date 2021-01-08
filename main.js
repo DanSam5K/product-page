734a0029e53e1ce38e04d654bdc094602d761c46
Daniel Samuel <dansam5000@gmail.com>
2021-01-08 15:21:53 +0100

    creating our hardcoded products and binding data to attribute

--- main.js
+++ main.js
@@ -2,6 +2,24 @@
 const app = new Vue({
     el: '#app',
     data: {
-        product: "Socks"
+        products: [
+            {
+                name: 'Four Colour Combo Socks',
+                image: './images/4colourcombosocks.jpg',
+                alt: 'four colour variety socks'
+            },
+            {
+                name: 'Three in One Socks',
+                image: './images/threeinonesocks.jpg',
+                alt: "3 contain in one pack socks"
+            },
+            {
+                name: 'Ash Socks',
+                image: './images/ashsocks.jpg',
+                alt: 'ash colour socks'
+            }
+        ]
+                    
     }
+        
 })
