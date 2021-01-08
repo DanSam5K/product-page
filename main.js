fae12f8fe604498068f2012f4b3cd116ae9e3700
Daniel Samuel <dansam5000@gmail.com>
2021-01-08 18:07:03 +0100

    conditional rendering instock or out of stock wiht vue if-else

--- main.js
+++ main.js
@@ -6,20 +6,27 @@ const app = new Vue({
             {
                 name: 'Four Colour Combo Socks',
                 image: './images/4colourcombosocks.jpg',
-                alt: 'four colour variety socks'
+                alt: 'four colour variety socks',
+                inStock: true,
+                onSale: true
             },
             {
                 name: 'Three in One Socks',
                 image: './images/threeinonesocks.jpg',
-                alt: "3 contain in one pack socks"
+                alt: '3 contain in one pack socks',
+                inStock: false,
+                onSale: false
             },
             {
                 name: 'Ash Socks',
                 image: './images/ashsocks.jpg',
-                alt: 'ash colour socks'
+                alt: 'ash colour socks',
+                inStock: true,
+                onSale: true
+
             }
-        ]
-                    
+        ],
+                       
     }
         
 })
