b7b88c19c49fb58e87c8eb2c0d1551372fb96143
Daniel Samuel <dansam5000@gmail.com>
2021-01-10 13:13:34 +0100

    starting a vue instance with single product

--- main.js
+++ main.js
@@ -1,32 +1,62 @@
 
 const app = new Vue({
-    el: '#app',
+    el: "#app",
     data: {
-        products: [
-            {
-                name: 'Four Colour Combo Socks',
-                image: './images/4colourcombosocks.jpg',
-                alt: 'four colour variety socks',
-                inStock: true,
-                onSale: true
-            },
-            {
-                name: 'Three in One Socks',
-                image: './images/threeinonesocks.jpg',
-                alt: '3 contain in one pack socks',
-                inStock: false,
-                onSale: false
-            },
-            {
-                name: 'Ash Socks',
-                image: './images/ashsocks.jpg',
-                alt: 'ash colour socks',
-                inStock: true,
-                onSale: true
-
-            }
-        ],
-                       
+        product: "Socks",
+        description: "A Set of warm, fuzzy multi-coloured Socks"
     }
+
+
+    // el: '#app',
+    // data: {
+    //     products: [
+    //         {
+    //             name: 'Four Colour Combo Socks',
+    //             image: './images/4colourcombosocks.jpg',
+    //             alt: 'four colour variety socks',
+    //             inStock: true,
+    //             onSale: true,
+    //             details: ['50% cotton', '50% polyester', 'Gender-unisex'],
+    //             variants: [
+    //                 {
+    //                     variantId: 1,
+    //                     variantColor: 'multi-color'
+    //                 }
+    //             ]
+
+    //         },
+    //         {
+    //             name: 'Three in One Socks',
+    //             image: './images/threeinonesocks.jpg',
+    //             alt: '3 contain in one pack socks',
+    //             inStock: false,
+    //             onSale: false,
+    //             details: ['40% cotton', '60% polyester', 'Gender-male'],
+    //             variants: [
+    //                 {
+    //                     variantId: 3,
+    //                     variantColor: 'Shade var Color'
+    //                 }
+    //             ]
+
+    //         },
+    //         {
+    //             name: 'Ash Socks',
+    //             image: './images/ashsocks.jpg',
+    //             alt: 'ash colour socks',
+    //             inStock: true,
+    //             onSale: true,
+    //             details: ['80% cotton', '20% polyester', 'Gender-unisex'],
+    //             variants: [
+    //                 {
+    //                     variantId: 3,
+    //                     variantColor: 'Ash Color'
+    //                 }
+    //                           ]
+
+    //         }
+    //     ],
+                       
+    // }
         
 })
