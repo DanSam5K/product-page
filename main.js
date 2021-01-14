774dd95c7e87e1be2d3d93449f8d3f806702d107
Daniel Samuel <dansam5000@gmail.com>
2021-01-14 12:43:15 +0100

    computed properties to calculate value rather than store

--- main.js
+++ main.js
@@ -3,43 +3,61 @@ const app = new Vue({
     el: "#app",
     data: {
         product: "Socks",
+        brand: "Master Vue With",
         description: "A Set of warm, fuzzy multi-coloured Socks",
-        image: "./images/4colourcombosocks.jpg",
+        selectedVariant: 0,
         altText: "four colour variety socks",
         linkRef: "https://www.amazon.com/Losa-Kute-Womens-Casual-Nolvety/dp/B07F31CXTC",
-        inStock: false,
         inventory: 0,
-        onSale: true,
-        details: ["80% cotton", "20% polyester", "Gender-Unisex", "Color-multiColor"],
+        details: ["80% cotton", "20% polyester", "Gender-Unisex"],
         variants: [
             {
                 variantId: 1,
-                variantColor: "blue",
-                variantImage: "./images/blue-socks.jpg"
+                variantColor: "multi-color",
+                variantImage: "./images/4colourcombosocks.jpg",
+                variantQuantity: 2,
+                variantSale: true
             },
             {
                 variantId: 2,
-                variantColor: "pink",
-                variantImage: "./images/Pink.jpg"
+                variantColor: "blue",
+                variantImage: "./images/blue-socks.jpg",
+                variantQuantity: 0,
+                variantSale: false
             },
             {
                 variantId: 3,
-                variantColor: "green",
-                variantImage: "./images/green.jpg"
+                variantColor: "pink",
+                variantImage: "./images/Pink.jpg",
+                variantQuantity: 11,
+                variantSale: true
             },
             {
                 variantId: 4,
-                variantColor: "yellow",
-                variantImage: "./images/yellow.jpg"
+                variantColor: "green",
+                variantImage: "./images/green.jpg",
+                variantQuantity: 0,
+                variantSale: false
             },
             {
                 variantId: 5,
+                variantColor: "yellow",
+                variantImage: "./images/yellow.jpg",
+                variantQuantity: 6,
+                variantSale: true
+            },
+            {
+                variantId: 6,
                 variantColor: "red",
-                variantImage: "./images/red.jpg"
+                variantImage: "./images/red.jpg",
+                variantImage: 0,
+                variantSale: false
+
             }
         ],
         sizes: ["size 37", "size 40", "size 42", "size 44"],
-        cart: 0
+        cart: 0,
+        onSale: true
     },
         //methods
     methods:{
@@ -47,11 +65,29 @@ const app = new Vue({
         this.cart +=1 ;
         },
 
-        updateProduct(variantImage){
-            this.image = variantImage;
+        updateProduct(index){
+            this.selectedVariant = index;
+            console.log(index)
         },
         removeFromCart() {
             this.cart -=1;
+        }
+    },
+
+    computed: {
+        title() {
+            return this.brand + " " + this.product + " Project" 
+        },
+        image() {
+            return this.variants[this.selectedVariant].variantImage
+        },
+        inStock() {
+            return this.variants[this.selectedVariant].variantQuantity;
+        },
+        sale() {
+            if(this.variants[this.selectedVariant].variantSale) {
+                return `${this.variants[this.selectedVariant].variantColor} ${this.product} are on sale!`
+            }return `${this.variants[this.selectedVariant].variantColor} ${this.product} are not on sale!`
         }
     }
      
